6b9084352e5a9b46bd75b008cbf7545c62477d02
alisson-moura <alisson.mo.moura@outlook.com.br>
2024-12-17 12:35:17 -0300

    fix: alterado tipo de migration

--- apps/api/scripts/run-e2e.sh
+++ apps/api/scripts/run-e2e.sh
@@ -8,6 +8,6 @@ docker compose up -d
 echo '🟡 - Waiting for database to be ready...'
 $DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
 
-dotenv -e .env.test -- npx prisma migrate deploy
+dotenv -e .env.test -- npx prisma migrate dev --name init
 
 npm run test:e2e
